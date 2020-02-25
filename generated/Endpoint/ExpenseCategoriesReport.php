c7a1ed2f982f83eac011d9a3659da80260d5c306
Xavier Lacot <xavier@lacot.org>
2020-02-25 18:23:32 +0100

    fixed the location of reports endpoints parameters

--- generated/Endpoint/ExpenseCategoriesReport.php
+++ generated/Endpoint/ExpenseCategoriesReport.php
@@ -7,11 +7,16 @@ class ExpenseCategoriesReport extends \Jane\OpenApiRuntime\Client\BaseEndpoint i
     /**
      * 
      *
-     * @param \JoliCode\Harvest\Api\Model\ReportsExpensesCategoriesGetBody $payload json payload
+     * @param array $queryParameters {
+     *     @var string $from Only report on expenses with a spent_date on or after the given date.
+     *     @var string $to Only report on expenses with a spent_date on or before the given date.
+     *     @var int $page The page number to use in pagination. For instance, if you make a list request and receive 100 records, your subsequent call can include page=2 to retrieve the next page of the list. (Default: 1)
+     *     @var int $per_page The number of records to return per page. Can range between 1 and 1000.  (Default: 1000)
+     * }
      */
-    public function __construct(\JoliCode\Harvest\Api\Model\ReportsExpensesCategoriesGetBody $payload)
+    public function __construct(array $queryParameters = array())
     {
-        $this->body = $payload;
+        $this->queryParameters = $queryParameters;
     }
     use \Jane\OpenApiRuntime\Client\Psr7EndpointTrait;
     public function getMethod() : string
@@ -24,7 +29,19 @@ class ExpenseCategoriesReport extends \Jane\OpenApiRuntime\Client\BaseEndpoint i
     }
     public function getBody(\Symfony\Component\Serializer\SerializerInterface $serializer, $streamFactory = null) : array
     {
-        return $this->getSerializedBody($serializer);
+        return array(array(), null);
+    }
+    protected function getQueryOptionsResolver() : \Symfony\Component\OptionsResolver\OptionsResolver
+    {
+        $optionsResolver = parent::getQueryOptionsResolver();
+        $optionsResolver->setDefined(array('from', 'to', 'page', 'per_page'));
+        $optionsResolver->setRequired(array('from', 'to'));
+        $optionsResolver->setDefaults(array());
+        $optionsResolver->setAllowedTypes('from', array('string'));
+        $optionsResolver->setAllowedTypes('to', array('string'));
+        $optionsResolver->setAllowedTypes('page', array('int'));
+        $optionsResolver->setAllowedTypes('per_page', array('int'));
+        return $optionsResolver;
     }
     /**
      * {@inheritdoc}
